88cffa12c6e086402a280b484d573a200789c329
Ekaterina <ketee.android@yahoo.com>
2021-03-26 07:30:00 +0300

    Homework 2 is done

--- app/src/main/java/com/ketee_jishs/nasa_app/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ketee_jishs/nasa_app/ui/settings/SettingsFragment.kt
@@ -1,13 +1,21 @@
 package com.ketee_jishs.nasa_app.ui.settings
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.ketee_jishs.nasa_app.R
+import com.ketee_jishs.nasa_app.util.KEY_THEME
+import com.ketee_jishs.nasa_app.util.THEME_DAY
+import com.ketee_jishs.nasa_app.util.THEME_NIGHT
+import com.ketee_jishs.nasa_app.util.THEME_PREFS_NAME
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment() {
+    private val sharedPrefs by lazy {activity?.getSharedPreferences(THEME_PREFS_NAME, Context.MODE_PRIVATE)}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +23,41 @@ class SettingsFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
+
+    override fun onStart() {
+        super.onStart()
+        changeTheme()
+        initTheme()
+    }
+
+    private fun changeTheme() {
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.radioButtonLightTheme -> setTheme(AppCompatDelegate.MODE_NIGHT_NO, THEME_DAY)
+                R.id.radioButtonDarkTheme -> setTheme(AppCompatDelegate.MODE_NIGHT_YES, THEME_NIGHT)
+            }
+        }
+    }
+
+    private fun setTheme (themeMode: Int, prefsMode: Int) {
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+        saveTheme(prefsMode)
+    }
+
+    private fun saveTheme(theme: Int) = sharedPrefs?.edit()?.putInt(KEY_THEME, theme)?.apply()
+
+    private fun initTheme() {
+        when (getSavedTheme()) {
+            THEME_DAY -> {
+                radioButtonLightTheme.isChecked = true
+            }
+            THEME_NIGHT -> {
+                radioButtonDarkTheme.isChecked = true
+            }
+        }
+    }
+
+    private fun getSavedTheme() = sharedPrefs?.getInt(KEY_THEME, THEME_DAY)
 
     companion object {
         fun newInstance() = SettingsFragment()
--- app/src/main/java/com/ketee_jishs/nasa_app/util/Constants.kt
+++ app/src/main/java/com/ketee_jishs/nasa_app/util/Constants.kt
@@ -1,7 +1,17 @@
 package com.ketee_jishs.nasa_app.util
 
 const val CHIPS_SHARED_PREFS_NAME = "CHIPS_SHARED_PREFS_NAME"
-const val CHIP_KEY = "TODAY_CHIP_KEY"
+const val KEY_CHIP = "TODAY_CHIP_KEY"
 const val CHECKED_TODAY = 1
 const val CHECKED_YESTERDAY = 2
 const val CHECKED_DAY_BEFORE_YESTERDAY = 3
+
+const val THEME_PREFS_NAME = "THEME_PREFS_NAME"
+const val KEY_THEME = "KEY_THEME"
+const val THEME_DAY = 0
+const val THEME_NIGHT = 1
+
+const val SCREEN_PREFS_NAME = "SCREEN_PREFS_NAME"
+const val KEY_SCREEN = "KEY_SCREEN"
+const val PICTURE_SCREEN = 0
+const val SETTINGS_SCREEN = 1
